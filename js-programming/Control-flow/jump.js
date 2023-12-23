0dcaeb0c65b001961f39ce8919399d9cb545b253
rajnish98 <rajnishpandey63@gmail.com>
2023-12-23 19:40:27 +0530

    js practice

---
+++ js-programming/Control-flow/jump.js
@@ -0,0 +1,12 @@
+function jump(num) {
+  var i = 0;
+
+while(i < num){
+    i++;
+    if(i % 2 != 0)
+    continue;
+    console.log(i)
+}
+}
+jump(10);
+
---
+++ js-programming/Control-flow/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+function jump(num) {
+  var i = 0;
+
+while(i < num){
+    i++;
+    if(i % 2 == 0)
+    continue;
+    console.log(i)
+}
+}
+jump(3);
