44465a0a6d2e049199dd32c3d60f96c660f12d07
rajnish98 <rajnishpandey63@gmail.com>
2023-12-23 19:00:52 +0530

    js loop and switch case statment problems

---
+++ js-programming/Control-flow/loop.js
@@ -0,0 +1,30 @@
+function loop(num) {
+
+  // while loop
+  // var i = 0;
+  // while (i < num) {
+  //   if (i % 2  != 0) {
+  //     console.log(i + " ")
+  //   }
+  //   i++;
+  // }
+// for loop condition
+
+// for(let i = 0; i <= num; i++){
+//   if (i % 2 != 0) {
+//     console.log(i + " ");
+//   }
+// }
+
+//  do while loop condition
+
+do {
+
+  i++;
+} while (i < num){
+  if (i % 2 != 0) {
+    console.log(i + " ");
+  }
+}
+}
+loop(10);
