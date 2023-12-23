0cf8008571cb7234c70dc60bc02fb4067b0534fa
rajnish98 <rajnishpandey63@gmail.com>
2023-12-23 18:25:20 +0530

    js practice

---
+++ js-programming/Control-flow/grade.js
@@ -0,0 +1,18 @@
+function grade(num){
+  if(num >= 90){
+    console.log("A");
+}
+else if(num >=80){
+    console.log("B")
+}
+else if(num >= 70){
+   console.log("C")
+}else if(num >= 60){
+    console.log("D")
+}else if(num >=50){
+    console.log("E")
+}else{
+    console.log("F")
+}
+}
+grade(60);
